c2eb896c5bc0005cc2b8f1eeba0724bbda038bb6
David Riley <Dril112191@gmail.com>
2023-02-08 16:34:12 -0500

    .

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,13 +4,16 @@ export default function Header({showReadlist, setShowReadlist}) {
 
     return(
         <div className='header'>
+            {/* The header-btns container holds the toggle button to switch between the search and reading list page */}
             <div className="header-btns">
                 <button onClick={() => setShowReadlist(!showReadlist)} className="header-btn">
+                    {/* The text of the button changes based on the current state of the showReadlist variable */}
                     {showReadlist ? "Search Books" : "My list"}
                 </button>
             </div>  
             <div className='header-text'>
-                <h1>Discover Your Next Favorite Book with Our Personalized Book Finder</h1>
+                <h4 className="discover-text">Discover Your Next Favorite Book</h4>
+                <h1>Personalized Book Finder</h1>
                 <h2>Your One-Stop Destination for Book Discovery</h2>
             </div>
         </div>
